e7f8d1ea7696395dbe02c99cf77982f991e2404c
Sanja Risteska <sanja.risteska@yahoo.com>
2016-04-15 14:02:08 +0200

    Added files via upload

---
+++ QuotientAndRemainder/src/QuotientAndRemainderApp.java
@@ -0,0 +1,16 @@
+
+public class QuotientAndRemainderApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number1 = 13;
+		int number2 = 3;
+
+		System.out.println("First number - " + number1);
+		System.out.println("Second number - " + number2);
+		System.out.println(number1 / number2 + "\t" + number1 % number2);
+
+	}
+
+}
