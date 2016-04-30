42ccba389d18499f969629f167c401a37af7ba30
Sanja Risteska <sanja.risteska@yahoo.com>
2016-04-30 17:30:38 +0200

    Delete MembershipRepository.java

--- Library_Spring/LibrarySpringification/src/main/java/com/seavus/dataaccess/MembershipRepository.java
+++
@@ -1,9 +0,0 @@
-package com.seavus.dataaccess;
-
-import com.seavus.domain.Member;
-import com.seavus.domain.Membership;
-
-public interface MembershipRepository {
-	public void save(Member member, Membership membership);
-	public Member findMember(Long id);
-}
